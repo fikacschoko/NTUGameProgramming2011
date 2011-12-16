8689cb130faa99d14739bd08d06a4845eee525cd
tsaiichi <tsaiichi.1@gmail.com>
2011-12-16 11:23:03 +0800

    喵嗚><

--- function.h
+++ function.h
@@ -1,9 +1,16 @@
 #include "TheFlyWin32.h"
+#include "action.h"
 
 bool actorChangePose( ACTORid , ACTIONid );
 
-//bool beenHit( ACTORid attacker , ACTORid defender, OurFrame frame );
+bool beenHit( ACTORid  , ACTORid , OurFrame );
 
 float twoObjectDis( OBJECTid , OBJECTid );
 
 float posObjectDis( float* , OBJECTid );
+
+float twoObjectAngle(  OBJECTid , OBJECTid );
+
+float twoObjectCross(  OBJECTid , OBJECTid );
+
+//float twoDirAngle( float* , float* );
